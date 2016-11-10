a14e830d861a2b1b9a92c0b8c2b44dba8e5629fe
MoutonSanglant <defresne.thierry@gmail.com>
2016-11-10 18:46:21 +0100

    fixed output

--- src/debug/ft_debug_error_free.c
+++ src/debug/ft_debug_error_free.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/05 20:19:56 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/10/28 20:05:02 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/11/10 17:33:48 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void			ft_debug_error_free(const char *fn_name)
 {
-	ft_putstr("\n\x1B[31mERROR in libft.a: the function ");
+	ft_putstr("\n\x1B[31mERROR in libft.a:\nthe function ");
 	ft_putstr(fn_name);
-	ft_putendl("returned the following error:\ncandidate for free is NULL. \
-			Verify your code, it is unsafe !!!\x1B[0m");
+	ft_putendl(" returned the following error: candidate for free is NULL.");
+	ft_putendl("Verify your code, it is unsafe !!!\x1B[0m");
 }
